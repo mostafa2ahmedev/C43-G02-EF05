b5c17fd369ce3489a7c00f99a8c05b7a57bb9645
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-03-16 17:04:28 +0200

    Demo

---
+++ Demo/Data/NorthwindContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Demo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Data;
+
+public partial class NorthwindContext : DbContext
+{
+    public NorthwindContext()
+    {
+    }
+
+    public NorthwindContext(DbContextOptions<NorthwindContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<ProductsAboveAveragePrice> ProductsAboveAveragePrices { get; set; }
+
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server = .; Database = Northwind; Trusted_Connection = True; TrustServerCertificate = True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.Property(e => e.CustomerId).IsFixedLength();
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.ReorderLevel).HasDefaultValue((short)0);
+            entity.Property(e => e.UnitPrice).HasDefaultValue(0m);
+            entity.Property(e => e.UnitsInStock).HasDefaultValue((short)0);
+            entity.Property(e => e.UnitsOnOrder).HasDefaultValue((short)0);
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products).HasConstraintName("FK_Products_Categories");
+
+            entity.HasOne(d => d.Supplier).WithMany(p => p.Products).HasConstraintName("FK_Products_Suppliers");
+        });
+
+        modelBuilder.Entity<ProductsAboveAveragePrice>(entity =>
+        {
+            entity.ToView("Products Above Average Price");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace Demo
+﻿using Demo.Data;
+using Demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Cross Join
+            using NorthwindContext dbContext = new NorthwindContext();
+
+
+
+
+            //var Result = from S in dbContext.Suppliers
+            //             from P in dbContext.Products
+            //             select new
+            //             {
+            //                 Supplier = S,
+            //                 Product = P
+            //             };
+
+            //  Result = dbContext.Suppliers
+            //         .SelectMany(S => dbContext.Products, (S, P) => new
+            //         {
+            //             Supplier = S,
+            //             Product = P
+            //         });
+
+
+
+
+
+            #endregion
+
+
+            #region Run SQL Query, Stored Procedures
+            //// Execute Select Statement // 1. FromSqlRaw - 2. FromSqlInterpolated
+
+            //var count = 3;
+            //var result = dbContext.Categories.FromSqlRaw("select top({0}) * from Categories", count);
+
+            //var categories = dbContext.Categories.FromSqlInterpolated($"select top({count}) * from Categories");
+
+            //// Execute Insert, Update, Delete Statement // 1. FromSqlRaw - 2. FromSqlInterpolated
+
+            //var catId = 1;
+            //dbContext.Database.ExecuteSqlInterpolated($"update Categories set CategoryName='TestCat' where CategoryID={catId}");
+
+            //NorthwindContextProcedures northwindContextProcedures = new NorthwindContextProcedures(dbContext);
+
+            //var res = northwindContextProcedures.SalesByCategoryAsync("TestCat", "2018").Result;
+
+            #endregion
+
         }
     }
 }
